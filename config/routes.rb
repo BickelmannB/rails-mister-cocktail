cd56459defce3d9e0c5906d810882cdd3ced315d
bickelmann brice <bickelmann.b@gmail.com>
2019-05-24 16:40:17 +0200

    cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create, :new]
+  end
+
+  resources :doses, only: [:destroy]
   # get 'cocktails', to: 'cocktails#index'
   # get 'cocktails/new', to: 'cocktails#new'
   # get 'cocktails/:id', to: 'cocktails#show', as: 'show'
